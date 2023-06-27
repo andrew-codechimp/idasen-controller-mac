42c527a269058712bfe3ebdb19f3ed6d49504f26
Andrew Jackson <andrew@codechimp.org>
2023-06-27 16:17:43 +0100

    Fix apple script

--- Desk Controller/AppleScript Commands/MoveDeskToHeightCommand.swift
+++ Desk Controller/AppleScript Commands/MoveDeskToHeightCommand.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-
 class MoveDeskToHeightCommand: NSScriptCommand {
     
     override func performDefaultImplementation() -> Any? {
@@ -18,36 +17,18 @@ class MoveDeskToHeightCommand: NSScriptCommand {
         
         var height: Float?
         
-        print("Move desk to: \(parameter)")
+        print("Move desk to height: \(parameter)")
         
-        switch parameter {
-        case "to-stand":
-            DeskController.shared?.moveToPosition(.stand)
-            break
-        case "to-sit":
-            DeskController.shared?.moveToPosition(.sit)
-            break
-        case "up":
-            DeskController.shared?.moveUp()
-            break
-        case "down":
-            DeskController.shared?.moveDown()
-            break
-            
-        default:
-            if parameter.hasSuffix("cm") {
-                height = Float(parameter.dropLast(2))
-            } else if parameter.hasSuffix("in") {
-                height = Float(parameter.dropLast(2))?.convertToCentimeters()
-            } else if let value = Float(parameter) {
-                height = Preferences.shared.isMetric ? value : value.convertToCentimeters()
-            }
-            
-            if let height = height {
-                DeskController.shared?.moveToHeight(height)
-            }
-            
-            break
+        if parameter.hasSuffix("cm") {
+            height = Float(parameter.dropLast(2))
+        } else if parameter.hasSuffix("in") {
+            height = Float(parameter.dropLast(2))?.convertToCentimeters()
+        } else if let value = Float(parameter) {
+            height = Preferences.shared.isMetric ? value : value.convertToCentimeters()
+        }
+        
+        if let height = height {
+            DeskController.shared?.moveToHeight(height)
         }
         
         return nil
